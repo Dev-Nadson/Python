3fe508b276a6046642a43501368fb985800eb9e8
Dev Nadson <nadson.pros@gmail.com>
2025-05-07 14:25:10 -0300

    Testando Funções

---
+++ Dicionario-Teste.py
@@ -0,0 +1,14 @@
+Personagem = {
+    'nome': 'nads',
+    'vida': '10',
+    'dano': '25',
+}
+
+Mobs = [
+    {'nome': 'zumbi', 'vida': '2', 'dano': '10',},
+    {'nome': 'esqueleto', 'vida': '7', 'dano': '7',},
+    {'nome': 'clown', 'vida': '10', 'dano': '15',}
+]
+
+print("Os dados do personagem são:", Personagem)
+print(f"Os Mobs são: \n{Mobs[0]['nome']}\n{Mobs[1]}\n{Mobs[2]} ")
