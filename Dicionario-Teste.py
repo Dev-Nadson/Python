a3f5a66edaa8313e0d5a3b6b0105e6c7fca6e565
Dev Nadson <nadson.pros@gmail.com>
2025-05-14 18:36:11 -0300

    Dicionário atualizado, inputs e listas adicionados

--- Dicionario-Teste.py
+++ Dicionario-Teste.py
@@ -1,14 +1,28 @@
+Nomes = []
+Vidas = []
+Danos = []
+i = 0
+nome = "a" #Para inicializar o while
+while nome != "end":
+    i += 1
+    nome = input(f"Insira o nome do {i}º Personagem: ")
+    Nomes.append(nome)
+    vida = input(f"Insira a vida do {i}º Personagem: ")
+    Vidas.append(vida)
+    dano = input(f"Insira o dano do {i}º Personagem: ")
+    Danos.append(dano)
+
 Personagem = {
-    'nome': 'nads',
-    'vida': '10',
-    'dano': '25',
+    'nome': Nomes,
+    'vida': Vidas,
+    'dano': Danos
 }
 
-Mobs = [
-    {'nome': 'zumbi', 'vida': '2', 'dano': '10',},
-    {'nome': 'esqueleto', 'vida': '7', 'dano': '7',},
-    {'nome': 'clown', 'vida': '10', 'dano': '15',}
-]
+# Mobs = [
+#     {'nome': 'zumbi', 'vida': '2', 'dano': '10',},
+#     {'nome': 'esqueleto', 'vida': '7', 'dano': '7',},
+#     {'nome': 'clown', 'vida': '10', 'dano': '15',}
+# ]
 
 print("Os dados do personagem são:", Personagem)
-print(f"Os Mobs são: \n{Mobs[0]['nome']}\n{Mobs[1]}\n{Mobs[2]} ")
+# print(f"Os Mobs são: \n{Mobs[0]['nome']}\n{Mobs[1]}\n{Mobs[2]} ")
