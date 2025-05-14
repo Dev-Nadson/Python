c1f4635e421c00c7fddcae225476d01dcfe0c067
Dev Nadson <nadson.pros@gmail.com>
2025-05-14 02:10:36 -0300

    Código otimizado e resumido com funções

--- Embaralhador.py
+++ Embaralhador.py
@@ -1,18 +1,19 @@
 import random
 
 word = input("Insira a palavra: ")
-lista = []
-new_word = ""
 
-tam = len(word)
-key = random.randint(0, tam)
-
-for i in range(tam):
-    lista.append(word[i]) #Transforma a string em lista facilitando a modificação
-
-random.shuffle(lista)
-
-for i in range(tam):
-    new_word += lista[i] #Transforma a lista em string
-
+lista = list(word) #Separa a Word na lista
+random.shuffle(lista) #Embaralha
+new_word = ''.join(lista) #Une a lista em string
 print(new_word)
+
+
+#Versão Manual Abaixo
+# lista = []
+# new_word = ""
+# tam = len(word)
+# key = random.randint(0, tam)
+# for i in range(tam):
+#     lista.append(word[i]) #Transforma a string em lista facilitando a modificação
+# for i in range(tam):
+#     new_word += lista[i] #Transforma a lista em string
