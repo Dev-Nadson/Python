377146de2f02797c8610a41d5702a4279d674893
Dev Nadson <nadson.pros@gmail.com>
2025-05-15 00:38:23 -0300

    Inputs de Mobs e Output Atualizado

--- Dicionario-Teste.py
+++ Dicionario-Teste.py
@@ -1,28 +1,63 @@
 Nomes = []
 Vidas = []
 Danos = []
-i = 0
-nome = "a" #Para inicializar o while
-while nome != "end":
-    i += 1
-    nome = input(f"Insira o nome do {i}º Personagem: ")
-    Nomes.append(nome)
-    vida = input(f"Insira a vida do {i}º Personagem: ")
-    Vidas.append(vida)
-    dano = input(f"Insira o dano do {i}º Personagem: ")
-    Danos.append(dano)
-
+NomesMb = []
+VidasMb = []
+DanosMb = []
 Personagem = {
     'nome': Nomes,
     'vida': Vidas,
     'dano': Danos
 }
+Mobs = {
+    'nome': NomesMb,
+    'vida': VidasMb,
+    'dano': DanosMb
+}
 
-# Mobs = [
-#     {'nome': 'zumbi', 'vida': '2', 'dano': '10',},
-#     {'nome': 'esqueleto', 'vida': '7', 'dano': '7',},
-#     {'nome': 'clown', 'vida': '10', 'dano': '15',}
-# ]
+def Personagens():
+    control = 0 #Para inicializar o while
+    i = 0
+    while control != 1:
+        i += 1
+        nome = input(f"Insira o nome do {i}º Personagem: ")
+        vida = input(f"Insira a vida do {i}º Personagem: ")
+        dano = input(f"Insira o dano do {i}º Personagem: ")
+        Nomes.append(nome)
+        Vidas.append(vida)
+        Danos.append(dano)
+        control = int(input("Insira qualquer número para continuar ou 1 Para finalizar: "))
 
-print("Os dados do personagem são:", Personagem)
-# print(f"Os Mobs são: \n{Mobs[0]['nome']}\n{Mobs[1]}\n{Mobs[2]} ")
+def MOBS():
+    control = 0 #Para inicializar o while
+    i = 0
+    while control != 1:
+        i += 1
+        nome = input(f"Insira o nome do {i}º Mob: ")
+        vida = input(f"Insira a vida do {i}º Mob: ")
+        dano = input(f"Insira o dano do {i}º Mob: ")
+        NomesMb.append(nome)
+        VidasMb.append(vida)
+        DanosMb.append(dano)
+        control = int(input("Insira qualquer número para continuar ou 1 Para finalizar: "))
+
+def ShowInfoChar():
+    print("\nDados dos Personagens\n")
+    for i in range(len(Nomes)):
+        print(f"Personagem {i+1}º: ")
+        print(f"  Nome : {Personagem['nome'][i]}")
+        print(f"  Vida : {Personagem['vida'][i]}") #Primeiro a chave depois o índice
+        print(f"  Dano : {Personagem['dano'][i]}")
+
+def ShowInfoMobs():
+    print("\nDados dos Mobs\n")
+    for n in range(len(Nomes)):
+        print(f"Mob {n+1}º: ")
+        print(f"  Nome : {Mobs['nome'][n]}")
+        print(f"  Vida : {Mobs['vida'][n]}") #Primeiro a chave depois o índice
+        print(f"  Dano : {Mobs['dano'][n]}")
+
+Personagens()
+ShowInfoChar()
+MOBS()
+ShowInfoMobs()
