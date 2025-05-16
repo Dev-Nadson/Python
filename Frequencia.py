7491e8994b1241f04eb06ff8653e267c6ee23d8e
Dev Nadson <nadson.pros@gmail.com>
2025-05-16 20:04:58 -0300

    Commit Inicial

---
+++ Frequencia.py
@@ -0,0 +1,6 @@
+from collections import Counter
+Frq = input("Enter the list: ").split(",") #Split ignora as vírgulas
+List = list(Frq)
+Cont = Counter(List)
+frequency = Cont.most_common(1) #Retorna uma Tupla ex:. '1': 4 -Número 1 quatro vezes-
+print(f"The most frequent number is: {frequency[0][0]}") #Aqui pega o primeiro valor
