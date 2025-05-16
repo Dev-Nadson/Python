e8dedc7c86f367f71a11958aecc62baa5ff0e347
Dev Nadson <nadson.pros@gmail.com>
2025-05-16 19:25:33 -0300

    Explicação inicial

--- Anagrama.py
+++ Anagrama.py
@@ -1,3 +1,6 @@
+import os
+os.system('cls')
+print("--- This algorithm checks if two words are palindromes ---")
 word1 = input("Enter the first word: ").lower()
 word2 = input("Enter the second word: ").lower()
 
@@ -7,7 +10,7 @@ cont = 0
 for i in w1:
     if i in w2:
         cont += 1
-if cont == len(w1) and len(w1) == len(w2) :
+if cont == len(w1) and len(w1) == len(w2):
     print("Is a palindrome")
 else:
-    print("Isnt a palindrome")
+    print("Isn't a palindrome")
