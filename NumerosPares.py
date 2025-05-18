0a0203b5575e758cc3599b7b8f8dbc62b9c14874
Dev Nadson <nadson.pros@gmail.com>
2025-05-18 19:52:50 -0300

    função adicionada e Lista de números

--- NumerosPares.py
+++ NumerosPares.py
@@ -1,3 +1,11 @@
-for a in range(0, 100, 2):
-    print(f"Os números são: {a}")
-    
+def Even(n1, n2):
+    for a in range(n1, (n2+1)):
+        if a % 2 == 0:
+            Numbers.append(a)
+
+Numbers = []
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+Even(n1, n2)
+print(Numbers)
+print(len(Numbers))
