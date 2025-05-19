02450b73a8bd36196b0b7eac6a1cd5d682b18a8a
Dev Nadson <nadson.pros@gmail.com>
2025-05-19 20:58:55 -0300

    Bug do \n corrigido

--- ExercicioArquivo.py
+++ ExercicioArquivo.py
@@ -5,27 +5,25 @@ ContWord = 0
 cont = []
 TextFull = []
 
-with open ("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/TextoGPT.txt", "r", encoding="utf-8") as arquivo:
+with open ("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/exemplo_01.txt", "r", encoding="utf-8") as arquivo:
     txt = arquivo.readlines()
 
 for i in range(len(txt)):
     NewText = list(txt[i].split(" "))
-    print(NewText) #Fix the \n Bugs
-    ContWord += len(NewText)
-    for content in NewText:
-        ContChar += len(content)
+    if "\n" in NewText:
+        NewText.remove("\n")
+    for char in NewText:
+        ContChar += len(char)
     TextFull += NewText
 
+ContWord += len(TextFull)
 cont = Counter(TextFull)
 cont = cont.most_common(1)
 
 print("The text is: ")
 for i in txt:
-    print('"', end="")
-    print(f'{i}"',)
-
+    print(f'{i}')
 
 print(f"\nThe number of words: {ContWord}")
 print(f"The number of characters: {ContChar}")
 print(f'The most frequent word is "{cont[0][0]}" appearing "{cont[0][1]}" times')
-
