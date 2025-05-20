b2fde2ada5ea144f1ed4cbbd06193000cb38552d
Dev Nadson <nadson.pros@gmail.com>
2025-05-20 18:47:34 -0300

    Correção no diretório

--- Manipulacao_Arquivos.py
+++ Manipulacao_Arquivos.py
@@ -1,24 +1,25 @@
+adress = "./ArquivosManipulacao/exemplo_02.txt"
 def read(): #Modo Leitura
-    with open("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/exemplo_01.txt", "r", encoding="utf-8") as archive:
+    with open(adress, "r", encoding="utf-8") as archive:
         txt = archive.readlines()
     for index, content in enumerate(txt):
         print(f"{index}. {content}", end="")
 
 def write(): #Modo Escrita, cria ou substitui arquvios
-    with open("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/exemplo_02.txt", "w", encoding="utf-8") as archive:
+    with open(adress, "w", encoding="utf-8") as archive:
         archive.write("Testando a escrita")
-    with open("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/exemplo_02.txt", "r", encoding="utf-8") as archive:   
+    with open(adress, "r", encoding="utf-8") as archive:   
         txt = archive.read()
         print(txt)
 
 def apend(): #Modo Adição, atualiza o arquvio
-    with open("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/exemplo_03.txt", "r", encoding="utf-8") as archive:
+    with open(adress, "r", encoding="utf-8") as archive:
         txt = archive.readlines()
     for index, content in enumerate(txt):
         print(f"{index}. {content}", end="")
-    with open("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/exemplo_03.txt", "a", encoding="utf-8") as archive:
+    with open(adress, "a", encoding="utf-8") as archive:
         archive.write("\nLinha 2")
-    with open("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/exemplo_03.txt", "r", encoding="utf-8") as archive:
+    with open(adress, "r", encoding="utf-8") as archive:
         txt = archive.readlines()
     for content in txt:
         print(f"{content}")
--- PandasTeste.py
+++ PandasTeste.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-txt = pd.read_csv("C:/Users/Ana Julia/Desktop/PRO-git/Python/ArquivosManipulacao/advertising.csv")
+txt = pd.read_csv("./ArquivosManipulacao/advertising.csv")
 print(txt)
