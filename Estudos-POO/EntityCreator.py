604f40bc3fc3a1cb4f9b0d40a4ec844371131ace
Dev Nadson <nadson.pros@gmail.com>
2025-05-26 20:09:41 -0300

    Commit Inicial

---
+++ Estudos-POO/EntityCreator.py
@@ -0,0 +1,18 @@
+class Characters:
+    def __init__(self, Name, Life, Damage):
+        self.Name = Name
+        self.Life = Life
+        self.Damage = Damage
+
+All_Data = []
+while True:
+    Name = input("Enter the name of the character: ")
+    if Name == "-1":
+        break
+    Life = input("Enter the life of the character")
+    Damage = input("Enter the damage of the character")
+    character = Characters(Name, Life, Damage)
+    character = character.Name, character.Life, character.Damage
+    All_Data.append(character)
+
+print(All_Data)
