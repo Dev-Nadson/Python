8f1367c161a9bf03e06c293d85b69b9bd2979ed2
Dev Nadson <nadson.pros@gmail.com>
2025-05-30 22:43:59 -0300

    Feat: Código base feito

--- Kivy_GUI/Index.py
+++ Kivy_GUI/Index.py
@@ -1,1 +1,10 @@
-print("Amamhã começamos")
+from kivy.app import App
+from kivy.lang import Builder
+
+UI = Builder.load_file("screen.kv")
+
+class Interface(App):
+    def build(self):
+        return UI
+    
+Interface().run()
